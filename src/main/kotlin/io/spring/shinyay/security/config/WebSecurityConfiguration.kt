72f953f1657cd2ae52af12e650be79e2ee80fb56
shinyay <shinya.com@gmail.com>
2022-03-02 23:26:23 +0900

    Add WebSecurityConfigurerAdapter

--- src/main/kotlin/io/spring/shinyay/security/config/WebSecurityConfiguration.kt
+++ src/main/kotlin/io/spring/shinyay/security/config/WebSecurityConfiguration.kt
@@ -7,5 +7,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfiguration : WebSecurityConfigurerAdapter(){
-    
+
 }
