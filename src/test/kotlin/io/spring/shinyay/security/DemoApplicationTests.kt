b8ac008c233e874268c133dcf5a098551c633f26
shinyay <shinya.com@gmail.com>
2022-03-02 23:10:33 +0900

    Initial Commit

---
+++ src/test/kotlin/io/spring/shinyay/security/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.spring.shinyay.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
