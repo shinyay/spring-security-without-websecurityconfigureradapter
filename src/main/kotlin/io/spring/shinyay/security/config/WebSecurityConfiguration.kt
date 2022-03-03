006236d0c213f7f77cb92b4dc76e75e5db3d7882
shinyay <shinya.com@gmail.com>
2022-03-03 13:49:35 +0900

    Modify WebSecurityConfigurerAdapter migration

--- src/main/kotlin/io/spring/shinyay/security/config/WebSecurityConfiguration.kt
+++ src/main/kotlin/io/spring/shinyay/security/config/WebSecurityConfiguration.kt
@@ -9,12 +9,13 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
+class WebSecurityConfiguration {
 
-    override fun configure(http: HttpSecurity?) {
+    fun filterChain(http: HttpSecurity?): SecurityFilterChain? {
         http
             // Do not restrict access to "/"
             ?.authorizeRequests()
@@ -33,10 +34,11 @@ class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
             ?.logout()
             ?.logoutUrl("/logout")
             ?.logoutSuccessUrl("/")
+        return http?.build()
     }
 
     @Bean
-    override fun userDetailsService(): UserDetailsService {
+    fun userDetailsService(): UserDetailsService {
         val user: UserDetails = User.withDefaultPasswordEncoder()
             .username("user")
             .password("password")
