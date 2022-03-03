dc10c08d04b42102b012b116a37159f9463757dc
shinyay <shinya.com@gmail.com>
2022-03-03 13:31:30 +0900

    Add current timestamp

--- src/main/kotlin/io/spring/shinyay/security/controller/SecuredController.kt
+++ src/main/kotlin/io/spring/shinyay/security/controller/SecuredController.kt
@@ -3,6 +3,9 @@ package io.spring.shinyay.security.controller
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @EnableWebSecurity
@@ -13,8 +16,8 @@ class SecuredController {
         return "Secured"
     }
 
-    @GetMapping("/home")
+    @GetMapping("/")
     fun home(): String {
-        return "Home"
+        return "Hello at ${ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))}"
     }
 }
